9f247fd448a7e92245742d78d40d7066a913dd67
Ayushi Shivhare <ayushishivhare18>
2024-08-28 19:02:48 +0530

    Initial commit

---
+++ src/models/userModel.js
@@ -0,0 +1,25 @@
+const {Schema, model} = require("mongoose");
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 4
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 5
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 4
+    },
+
+});
+const userModel = model("users", userSchema);
+module.exports = userModel;
